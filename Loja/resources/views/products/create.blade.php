0d98cd252f44907ce81f107e8c2d6dae767d3c27
Cristian martini <cristianmartini.sp@gmail.com>
2023-10-25 20:40:33 -0300

    usuario autenticaçao

--- Loja/resources/views/products/create.blade.php
+++ Loja/resources/views/products/create.blade.php
@@ -4,24 +4,24 @@
     @csrf
     <div>
         <label for="title">Nome do Produto</label>
-        <input type="text" name="name" 
+        <input type="text" name="name"
                placeholder="Digite o nome do Produto" value="{{ old('name') }}">
-        <input type="text"  name="description" 
+        <input type="text"  name="description"
                placeholder="Descriçao" value="{{ old('description') }}">
-        <input type="text"  name="price" 
+        <input type="text"  name="price"
                placeholder="Preço do  Produto" value="{{ old('pri
                ce') }}">
 
     </div>
-    
+
     <div>
-       
+
         <select name="category_id" >
             @foreach ($categories as $category)
         <option value="{{ $category->id }}">{{"$category->name"}}</option>
     </div>@endforeach
     </select>
-        
-    
+
+
     <button type="submit">Criar produto</button>
 </form>
